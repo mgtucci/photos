fa4781bc9ccec0ebf7d6a3decddf6be82ac50f04
Michael Tucci <michaelgabrieltucci@gmail.com>
2022-01-04 12:24:27 -0600

    rename photos to index

--- compile_site.sh
+++ compile_site.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-sed 's/INSERT_HEADER/$x/g' photos_base.html | x="$(<header.html)" envsubst '$x' > photos.html
+sed 's/INSERT_HEADER/$x/g' index_base.html | x="$(<header.html)" envsubst '$x' > index.html
 sed 's/INSERT_HEADER/$x/g' video_base.html | x="$(<header.html)" envsubst '$x' > video.html
 sed 's/INSERT_HEADER/$x/g' contact_base.html | x="$(<header.html)" envsubst '$x' > contact.html
 sed 's/INSERT_HEADER/$x/g' music_base.html | x="$(<header.html)" envsubst '$x' > music.html 
